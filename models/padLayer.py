ba2c303f890fb1e2e2cdb65df28efd86ab1b1cab
simondparsons <s.d.parsons@gmail.com>
2026-01-28 00:05:51 +0000

    ResNet18 working with all identity operations

--- models/padLayer.py
+++ models/padLayer.py
@@ -3,9 +3,14 @@
 # Simon Parsons
 # 26-01-26
 #
+# As in the rest of the code, the key reference is:
+# He, K., Zhang, X., Ren, S., & Sun, J. (2016). Deep residual learning
+# for image recognition. In Proceedings of the IEEE Conference on
+# Computer Vision and Pattern Recognition (pp. 770–778).
+# 
 # Often in a ResNet we need to combine the output of layers with
-# different nmebers of channels (becausethey use different numbers of
-# filters. This class makes that happen.
+# different numbers of channels (because they use different numbers of
+# filters). This class makes that happen.
 #
 # As explained in He et al., Section 3.2, we need to create a linear
 # projection of x to make this happen, and as:
@@ -16,8 +21,10 @@
 # https://github.com/tflearn/tflearn/blob/master/tflearn/layers/conv.py#L1590
 #
 # provides code for that (see below). Since we use tf.pad() and this
-# is tf function, we can't call it direct from Keras (says the
+# is a tf function, we can't call it direct from Keras (says the
 # interpreter) and so we have to wrap it in a Keras layer.
+#
+# Note also the comments under "Residual Network" in He et al. about zero padding.
 
 from keras import layers
 from keras.layers import Layer
